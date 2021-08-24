40b94fbf1ff04cdf0a0b28f31e2f6125390a62ec
LzGrin <lizaveta.grinyaeva@gmail.com>
2021-08-24 16:45:51 +0300

    new

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PageTitleComponent } from "./common/page-title/page-title.component";
+import { EventPageComponent } from "./common/event-page/event-page.component";
 
-const routes: Routes = [{ path: "", component: PageTitleComponent, pathMatch: "full" }];
+const routes: Routes = [
+  {
+    path: "event",
+    component: EventPageComponent
+  },
+  { path: "", component: PageTitleComponent, pathMatch: "full" }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
