e3f205aa5b4b49ba0a307b5ed44c1507f59d3a05
LzGrin <lizaveta.grinyaeva@gmail.com>
2021-08-26 16:09:32 +0300

    new

--- apps/frontend/src/app/common/event-page/event-page.component.ts
+++ apps/frontend/src/app/common/event-page/event-page.component.ts
@@ -10,45 +10,45 @@ import { FormInfo } from "../../models/formInfo";
   styleUrls: ["./event-page.component.less"]
 })
 export class EventPageComponent {
-  save: string;
-  cancel: string;
-  title: string;
-  public FormInfo: FormInfo;
-  @Input()
-  input: string;
-  public formG: FormGroup;
-  inputControl: FormControl = new FormControl();
-  id: string;
-
-  constructor(private service: EventService, private route: ActivatedRoute, private fb: FormBuilder) {
-    this.formG = fb.group({
-      name: "",
-      place: "",
-      data: "",
-      textarea: ""
-    });
-  }
-  ngOnInit(): void {
-    this.fb.group(FormGroup);
-    this.formG.valueChanges.subscribe(value => {
-      value;
-    });
-    this.route.params.subscribe(param => {
-      const id = param.id || "";
-      this.service.getValue(id);
-    });
-    this.formG.patchValue({
-      name: this.FormInfo.name,
-      place: this.FormInfo.place,
-      data: this.FormInfo.data,
-      textarea: this.FormInfo.textarea
-    });
-  }
-
-  changesInfo() {
-    this.FormInfo.name = this.formG.get("name").value;
-    this.FormInfo.place = this.formG.get("place").value;
-    this.FormInfo.data = this.formG.get("data").value;
-    this.FormInfo.textarea = this.formG.get("textarea").value;
-  }
+  //   save: string;
+  //   cancel: string;
+  //   title: string;
+  //   public FormInfo: FormInfo;
+  //   @Input()
+  //   input: string;
+  //   public formG: FormGroup;
+  //   inputControl: FormControl = new FormControl();
+  //   id: string;
+  //
+  //   constructor(private service: EventService, private route: ActivatedRoute, private fb: FormBuilder) {
+  //     this.formG = fb.group({
+  //       name: "",
+  //       place: "",
+  //       data: "",
+  //       textarea: ""
+  //     });
+  //   }
+  //   ngOnInit(): void {
+  //     this.fb.group(FormGroup);
+  //     this.formG.valueChanges.subscribe(value => {
+  //       value;
+  //     });
+  //     this.route.params.subscribe(param => {
+  //       const id = param.id || "";
+  //       this.service.getValue(id);
+  //     });
+  //     this.formG.patchValue({
+  //       name: this.FormInfo.name,
+  //       place: this.FormInfo.place,
+  //       data: this.FormInfo.data,
+  //       textarea: this.FormInfo.textarea
+  //     });
+  //   }
+  //
+  //   changesInfo() {
+  //     this.FormInfo.name = this.formG.get("name").value;
+  //     this.FormInfo.place = this.formG.get("place").value;
+  //     this.FormInfo.data = this.formG.get("data").value;
+  //     this.FormInfo.textarea = this.formG.get("textarea").value;
+  //   }
 }
