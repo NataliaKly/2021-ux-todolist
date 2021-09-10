8516564f769226e412108643533656f8609edba2
LzGrin <lizaveta.grinyaeva@gmail.com>
2021-09-10 20:43:58 +0300

    new

--- apps/frontend/src/app/common/event-page/event-page.component.ts
+++ apps/frontend/src/app/common/event-page/event-page.component.ts
@@ -17,43 +17,38 @@ export class EventPageComponent {
     place: "",
     description: ""
   };
-  public form: EventDto;
   public pageId: string;
-  public pageForm: EventDto;
   public date: Date;
   constructor(private route: ActivatedRoute, private eventService: EventService) {}
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.pageId = params.id;
       this.eventService.getPageById(this.pageId).subscribe((pageForm: EventDto) => {
-        this.pageForm = pageForm;
-        this.formInfo.title = this.pageForm?.title;
-        this.formInfo.place = this.pageForm?.place;
-        this.date = new Date(this.pageForm?.date);
+        this.formInfo.title = pageForm?.title;
+        this.formInfo.place = pageForm?.place;
+        this.date = new Date(pageForm?.date);
         const [hours, minutes] = [this.date.getHours(), this.date.getMinutes()];
         const formattedTime: string = hours + ":" + minutes;
         this.formInfo.time = formattedTime;
-        const info: EventDto = {
-          title: this.formInfo.title,
-          place: this.formInfo.place,
-          date: this.formInfo.date,
-          id: this.pageId,
-          description: this.formInfo.description
-        };
-        const time = this.formInfo.time.split(":");
-        const timeHours = Number.parseInt(time[0]);
-        const timeMinutes = Number.parseInt(time[1]);
-        this.date.setHours(timeHours);
-        this.date.setMinutes(timeMinutes);
-        this.pageForm.date = this.date.toISOString();
-        this.pageForm = info;
       });
     });
   }
 
   saveInfo() {
-    this.eventService.patchSaveInfo(this.pageId, this.pageForm).subscribe((info: EventDto) => {
-      this.pageForm = info;
+    const time = this.formInfo.time.split(":");
+    const timeHours = Number.parseInt(time[0]);
+    const timeMinutes = Number.parseInt(time[1]);
+    this.date.setHours(timeHours);
+    this.date.setMinutes(timeMinutes);
+    const info: EventDto = {
+      title: this.formInfo.title,
+      place: this.formInfo.place,
+      date: this.date.toISOString(),
+      id: this.pageId,
+      description: this.formInfo.description
+    };
+    this.eventService.patchSaveInfo(this.pageId, info).subscribe((newInfo: EventDto) => {
+      newInfo;
     });
   }
 }
