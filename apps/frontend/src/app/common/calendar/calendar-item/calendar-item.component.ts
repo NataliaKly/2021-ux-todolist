ed0ce4a3ea39f974c541781efbfb089bc70bfd4c
elgr1021 <elizaveta-grinyaeva.mail.ru>
2021-10-11 18:50:18 +0400

    panel.component

--- apps/frontend/src/app/common/calendar/calendar-item/calendar-item.component.ts
+++ apps/frontend/src/app/common/calendar/calendar-item/calendar-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 import { FormInfoModel } from "../../../models/formInfo.model";
-import { PanelComponent } from "../../panel/panel.component";
 
 @Component({
   selector: "b-calendar-item",
